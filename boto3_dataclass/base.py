69f8ff1c6879b31df5d1c6a7d648968303c6edb2
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-22 15:13:15 -0400

    Refactor Base class and add make_one factory method
    Replaces the 'new' class method with 'make_one', which returns None if input data is None. Adds commented-out 'make_many' method for batch instantiation and updates the example usage accordingly.

--- boto3_dataclass/base.py
+++ boto3_dataclass/base.py
@@ -4,7 +4,7 @@
 Base class for all generated dataclasses.
 """
 
-from typing import Generic, TypeVar, Self
+from typing import Generic, TypeVar, Iterable, Union, List, Self
 import dataclasses
 
 T_MODEL = TypeVar("T_MODEL")
@@ -15,8 +15,14 @@ class Base(Generic[T_MODEL]):
     _data: T_MODEL = dataclasses.field()
 
     @classmethod
-    def new(cls, data: T_MODEL):
+    def make_one(cls, data: T_MODEL | None):
+        if data is None:
+            return None
         return cls(_data=data)
+
+    # @classmethod
+    # def make_many(cls, data: Iterable[T_MODEL]):
+    #     return [cls(_data=dct) for dct in data]
 
     @property
     def raw_data(self) -> T_MODEL:
@@ -36,8 +42,16 @@ if __name__ == "__main__":
         def special_user_attr(self):
             return self._data["special_user_attr"]
 
-    user = User.new({"special_user_attr": 123})
+        def user_method(self):
+            pass
 
+    user = User.make_one({"special_user_attr": 123})
     print(f"{user.raw_data = }")
     print(f"{user.special_user_attr = }")
+    _ = user.user_method()  # type hint works
 
+    # users = User.make_many([{"special_user_attr": 123}])
+    # user = users[0]
+    # print(f"{user.raw_data = }")
+    # print(f"{user.special_user_attr = }")
+    # _ = user.user_method()  # type hint NOT work
