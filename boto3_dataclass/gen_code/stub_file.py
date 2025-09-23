46708d4eb0a54b939bfdf091e45d7b93fe750c9c
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-22 23:11:50 -0400

    Update stub_file.py

--- boto3_dataclass/gen_code/stub_file.py
+++ boto3_dataclass/gen_code/stub_file.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+"""
+This module defines the Boto3Stubs dataclass for discovering and managing
+type definition stub files (type_defs.pyi) for AWS services installed as
+mypy_boto3_* packages in the current Python environment.
+
+It provides functionality to:
+
+- Retrieve the path to a type definition stub file for a specific AWS service.
+- List all available mypy_boto3_* stubs in the site-packages directory.
+"""
+
 import site
 import dataclasses
 from functools import cached_property
@@ -17,9 +28,52 @@ class Boto3Stubs:
     service: str = dataclasses.field()
 
     @cached_property
-    def path_type_def_stub_file(self) -> Path:
+    def dir_folder(self) -> Path:
         """
-        Get the path to the type definition stub file (type_defs.pyi) for a given package or module name.
+        Get the directory path for a given package or module name.
+
+        Example: ``site-packages/mypy_boto3_ec2``
         """
         folder = f"mypy_boto3_{self.service}"
-        return dir_site_packages.joinpath(folder, "type_defs.pyi")
+        return dir_site_packages / folder
+
+    @cached_property
+    def path_literals_stub_file(self) -> Path:
+        """
+        Get the path to the literals stub file (literals.pyi).
+
+        Example: ``site-packages/mypy_boto3_ec2/literals.pyi``
+        """
+        return self.dir_folder / "literals.pyi"
+
+    @cached_property
+    def path_type_def_stub_file(self) -> Path:
+        """
+        Get the path to the type definition stub file (type_defs.pyi).
+
+        Example: ``site-packages/mypy_boto3_ec2/type_defs.pyi``
+        """
+        return self.dir_folder / "type_defs.pyi"
+
+    @cached_property
+    def path_client_stub_file(self) -> Path:
+        """
+        Get the path to the client stub file (client.pyi).
+
+        Example: ``site-packages/mypy_boto3_ec2/client.pyi``
+        """
+        return self.dir_folder / "client.pyi"
+
+    @classmethod
+    def list_all(cls):
+        """
+        List all available mypy_boto3_* stubs in the site-packages directory.
+
+        :return: A list of Boto3Stubs instances for each discovered service.
+        """
+        boto3_stubs_list = list()
+        for path in dir_site_packages.iterdir():
+            if path.name.startswith("mypy_boto3_"):
+                service = path.name.removeprefix("mypy_boto3_")
+                boto3_stubs_list.append(cls(service=service))
+        return boto3_stubs_list
