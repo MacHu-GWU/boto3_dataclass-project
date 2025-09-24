3e80417c396048f8d7f84cc630b0a254f93a2eee
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-23 20:06:22 -0400

    Update test scripts for boto3 stubs and lambda
    Switched to using TypedDictDefMappingParser in test_gen_code_for_boto3_stubs.py and updated the service variable. In test_lambda.py, enabled printing of the Tags attribute for better visibility during testing.

--- scripts/s2_test_gen_code_for_boto3_stubs/test_gen_code_for_boto3_stubs.py
+++ scripts/s2_test_gen_code_for_boto3_stubs/test_gen_code_for_boto3_stubs.py
@@ -2,14 +2,17 @@
 
 from pathlib import Path
 from boto3_dataclass.gen_code.stub_file import Boto3Stubs
-from boto3_dataclass.gen_code.parser import Parser
+from boto3_dataclass.gen_code.type_defs_parser import TypedDictDefMappingParser
 
-# service = "lambda"
-service = "bedrock_runtime"
+# ------------------------------------------------------------------------------
+service = "lambda"
+# service = "bedrock_runtime"
+# ------------------------------------------------------------------------------
+
 boto3_stubs = Boto3Stubs(service=service)
 path_stub_file = boto3_stubs.path_type_def_stub_file
 print(path_stub_file)
-parser = Parser(path_stub_file=path_stub_file)
+parser = TypedDictDefMappingParser(path_stub_file=path_stub_file)
 tddm = parser.parse()
 code = tddm.gen_code(
     type_defs_line=f"from mypy_boto3_{service} import type_defs",
