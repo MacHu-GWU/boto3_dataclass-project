782b21d11b63683f1030457875adad4107b247d2
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-24 18:06:10 -0400

    Add templates and update template_enum for package structure
    Added LICENSE.txt.jinja, pyproject.toml.jinja, and __init__.py.jinja templates for boto3_dataclass_service. Moved and renamed type_defs.py.jinja, typed_dict_def.jinja, and typed_dict_field.jinja into a new package directory. Updated TemplateEnum to reference new and moved templates. Also updated count_code.py with a summary of service statistics.

--- boto3_dataclass/templates/template_enum.py
+++ boto3_dataclass/templates/template_enum.py
@@ -16,16 +16,28 @@ class TemplateEnum:
         return load_template("template_enum.py.jinja")
     
     @cached_property
-    def boto3_dataclass_service__typed_dict_def(self):
-        return load_template("boto3_dataclass_service/typed_dict_def.jinja")
+    def boto3_dataclass_service__LICENSE_txt(self):
+        return load_template("boto3_dataclass_service/LICENSE.txt.jinja")
     
     @cached_property
-    def boto3_dataclass_service__typed_dict_field(self):
-        return load_template("boto3_dataclass_service/typed_dict_field.jinja")
+    def boto3_dataclass_service__pyproject_toml(self):
+        return load_template("boto3_dataclass_service/pyproject.toml.jinja")
     
     @cached_property
-    def boto3_dataclass_service__type_defs_py(self):
-        return load_template("boto3_dataclass_service/type_defs.py.jinja")
+    def boto3_dataclass_service__package__typed_dict_def(self):
+        return load_template("boto3_dataclass_service/package/typed_dict_def.jinja")
+    
+    @cached_property
+    def boto3_dataclass_service__package__typed_dict_field(self):
+        return load_template("boto3_dataclass_service/package/typed_dict_field.jinja")
+    
+    @cached_property
+    def boto3_dataclass_service__package____init___py(self):
+        return load_template("boto3_dataclass_service/package/__init__.py.jinja")
+    
+    @cached_property
+    def boto3_dataclass_service__package__type_defs_py(self):
+        return load_template("boto3_dataclass_service/package/type_defs.py.jinja")
     
 
 tpl_enum = TemplateEnum()
