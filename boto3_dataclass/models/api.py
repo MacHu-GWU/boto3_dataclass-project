b3db701d6baf3d4b75b2d1e07c4770469c75ab4e
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-25 13:03:23 -0400

    Add API modules for models, publish, and templates
    Introduced new api.py files in models, publish, and templates packages to provide centralized imports for key classes and functions. This improves package structure and simplifies external imports.

---
+++ boto3_dataclass/models/api.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .typed_dict import NESTED_TYPE_SUBSCRIPTOR
+from .typed_dict import TypedDictFieldAnnotation
+from .typed_dict import field_name_mapping
+from .typed_dict import TypedDictField
+from .typed_dict import TypedDictDef
+from .typed_dict import TypedDefsModule
+from .caster import CasterMethod
+from .caster import CasterModule
