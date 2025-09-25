e43993ac87aa035aff9b05993b604b9cdce54f1b
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-25 14:27:08 -0400

    Update generate_template_enum_py_file.py

--- scripts/generate_template_enum_py_file.py
+++ scripts/generate_template_enum_py_file.py
@@ -4,6 +4,9 @@
 Use this script to generate the
 `boto3_dataclass/templates/template_enum.py <https://github.com/MacHu-GWU/boto3_dataclass-project/blob/main/boto3_dataclass/templates/template_enum.py>`_
 file containing an enumeration of all templates.
+
+It uses the :func:`gen_code` defined in
+`boto3_dataclass/templates/template_helpers.py <https://github.com/MacHu-GWU/boto3_dataclass-project/blob/main/boto3_dataclass/templates/template_helpers.py>`_
 """
 
 from boto3_dataclass.templates.template_helpers import gen_code
