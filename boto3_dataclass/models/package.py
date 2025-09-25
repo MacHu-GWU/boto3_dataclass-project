d548a68b03e87e22084970a411db213d4c6ea99e
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-24 20:12:42 -0400

    Refactor Package to use Service object for paths
    Updated the Package dataclass to accept a Service object instead of a string for the service attribute. Path calculations for pyproject.toml, __init__.py, and type_defs.py now use properties from the Service object, simplifying path management and improving code maintainability.

--- boto3_dataclass/models/package.py
+++ boto3_dataclass/models/package.py
@@ -4,9 +4,9 @@ import dataclasses
 from pathlib import Path
 from functools import cached_property
 
-from ..paths import path_enum
 from ..utils import write
 from ..templates.template_enum import tpl_enum
+from ..structures.service import Service
 
 
 @dataclasses.dataclass
@@ -15,41 +15,21 @@ class Package:
     Represents a generated boto3 dataclass package for a specific AWS service and version.
     """
 
-    service: str
+    service: Service
     version: str
 
-    @cached_property
-    def name(self) -> str:
-        mapper = {
-            "lambda": "awslambda",
-        }
-        service = mapper.get(self.service, self.service)
-        return f"boto3_dataclass_{service}"
-
-    @cached_property
-    def name_slug(self) -> str:
-        return self.name.replace("_", "-")
-
-    @cached_property
-    def dir_repo(self) -> Path:
-        return path_enum.dir_project_root / "build" / "repos" / f"{self.name}-project"
-
-    @cached_property
-    def dir_package(self) -> Path:
-        return self.dir_repo / self.name
-
     def write_pyproject_toml(self):
-        path = self.dir_repo / "pyproject.toml"
+        path = self.service.path_boto3_dataclass_pyproject_toml
         s = tpl_enum.boto3_dataclass_service__pyproject_toml.render(package=self)
         write(path, s)
 
     def write_init_py(self):
-        path = self.dir_package / "__init__.py"
+        path = self.service.path_boto3_dataclass_init_py
         s = tpl_enum.boto3_dataclass_service__package____init___py.py.render(
             package=self
         )
         write(path, s)
 
     def write_type_defs_py(self):
-        path = self.dir_package / "type_defs.py"
+        path = self.service.path_boto3_dataclass_type_defs_py
         # TODO
