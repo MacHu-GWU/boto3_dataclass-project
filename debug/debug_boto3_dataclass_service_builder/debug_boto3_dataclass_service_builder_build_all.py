179d9740a2c4833908022c9528b85ac0210feaac
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-26 01:58:53 -0400

    Add todo count printout and update build options
    Prints the number of remaining todo packages before running the build. Also updates commented-out build and upload method calls for clarity and flexibility.

--- debug/debug_boto3_dataclass_service_builder/debug_boto3_dataclass_service_builder_build_all.py
+++ debug/debug_boto3_dataclass_service_builder/debug_boto3_dataclass_service_builder_build_all.py
@@ -14,12 +14,17 @@ if __name__ == "__main__":
     package_status_loader = PackageStatusLoader()
     package_status_info = package_status_loader.read_cache()
     # package_status_info = package_status_loader.refresh_cache()
+    n_package = len(package_status_info)
+    n_todo = n_package - sum(package_status_info.values())
+    print(f"{n_todo} / {n_package} todo")
     # package_status_info = None
 
+
     limit = None
-    # limit = 3
+    # limit = 1
 
     # fmt: off
     Boto3DataclassServiceBuilder.parallel_build_all(package_status_info=package_status_info, limit=limit)
-    # Boto3DataclassServiceBuilder.parallel_upload_all(package_status_info=package_status_info, limit=limit)
+    # Boto3DataclassServiceBuilder.parallel_poetry_build_all(package_status_info=package_status_info, limit=limit)
+    # Boto3DataclassServiceBuilder.sequence_upload_all(package_status_info=package_status_info, limit=limit)
     # fmt: on
